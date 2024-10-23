1b47d529fab4484cae6ad44fe5b9fd22d14b193f
NoeOnDev <alxg5516@gmail.com>
2024-10-22 23:06:10 -0600

    Agregar propiedades y métodos a la clase User

--- src/users/domain/User.ts
+++ src/users/domain/User.ts
@@ -1,1 +1,63 @@
-export class User {}
+import { Contact } from "../../contacts/domain/Contact";
+
+export class User {
+  private id: string;
+  private username: string;
+  private password: string;
+  private contact: Contact;
+  private verified: Date | null;
+
+  constructor(
+    username: string,
+    password: string,
+    contact: Contact,
+    id?: string,
+    verified: Date | null = null
+  ) {
+    this.id = id || crypto.randomUUID();
+    this.username = username;
+    this.password = password;
+    this.contact = contact;
+    this.verified = verified;
+  }
+
+  getId(): string {
+    return this.id;
+  }
+
+  getUsername(): string {
+    return this.username;
+  }
+
+  getPassword(): string {
+    return this.password;
+  }
+
+  getEmail(): string {
+    return this.contact.getEmail();
+  }
+
+  getPhone(): string {
+    return this.contact.getPhone();
+  }
+
+  promoteToUser(): void {
+    this.contact.promoteToUser();
+  }
+
+  isContactPromotedToUser(): boolean {
+    return this.contact.getStatus() === "USER";
+  }
+
+  verifyUser(): void {
+    this.verified = new Date();
+  }
+
+  isVerified(): boolean {
+    return this.verified !== null;
+  }
+
+  getVerificationDate(): Date | null {
+    return this.verified;
+  }
+}
