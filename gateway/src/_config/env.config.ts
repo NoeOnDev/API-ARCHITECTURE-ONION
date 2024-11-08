0c22dd7d7f4b3db1a51d4f37f1fa04cc09446d43
NoeOnDev <alxg5516@gmail.com>
2024-11-08 04:44:35 -0600

    Agregar configuración del entorno y estructura inicial para el gateway

---
+++ gateway/src/_config/env.config.ts
@@ -0,0 +1,38 @@
+import { config } from "dotenv";
+import Joi from "joi";
+
+config();
+
+const envSchema = Joi.object({
+  PORT: Joi.number().required(),
+  USERS_SERVICE_URL: Joi.string().uri().required(),
+  NOTIFICATIONS_SERVICE_URL: Joi.string().uri().required(),
+}).unknown();
+
+const { error, value: envVars } = envSchema.validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+const { PORT, USERS_SERVICE_URL, NOTIFICATIONS_SERVICE_URL } = envVars;
+
+interface Env {
+  port: {
+    PORT: number;
+  };
+  services: {
+    USERS_SERVICE_URL: string;
+    NOTIFICATIONS_SERVICE_URL: string;
+  };
+}
+
+export const env: Env = {
+  port: {
+    PORT: PORT,
+  },
+  services: {
+    USERS_SERVICE_URL: USERS_SERVICE_URL,
+    NOTIFICATIONS_SERVICE_URL: NOTIFICATIONS_SERVICE_URL,
+  },
+};
