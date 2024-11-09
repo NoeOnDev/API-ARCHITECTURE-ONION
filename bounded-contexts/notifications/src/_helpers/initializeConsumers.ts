9755d0d44a1d2c62a1e371460678783ea35c72cc
NoeOnDev <alxg5516@gmail.com>
2024-11-09 12:08:50 -0600

    refactor: eliminar consumidores de notificaciones no utilizados y simplificar la inicialización

--- bounded-contexts/notifications/src/_helpers/initializeConsumers.ts
+++ bounded-contexts/notifications/src/_helpers/initializeConsumers.ts
@@ -1,8 +1,5 @@
 import { createRabbitMQChannel } from "../_config/rabbitmq.config";
 
-import { UserCreatedConsumer } from "../notifications/infrastructure/consumers/UserCreatedConsumer";
-import { ContactCreatedConsumer } from "../notifications/infrastructure/consumers/ContactCreatedConsumer";
-import { UserWelcomeConsumer } from "../notifications/infrastructure/consumers/UserWelcomeConsumer";
 import { ServiceNotificationConsumer } from "../notifications/infrastructure/consumers/ServiceNotificationConsumer";
 
 import { generateTokenForUser } from "../tokens/infrastructure/dependencyInjection";
@@ -11,30 +8,11 @@ import { sendNotification } from "../notifications/infrastructure/dependencyInje
 export const initializeConsumers = async () => {
   const channel = await createRabbitMQChannel();
 
-  const userCreatedConsumer = new UserCreatedConsumer(
-    channel,
-    generateTokenForUser,
-    sendNotification
-  );
-
-  const contactCreatedConsumer = new ContactCreatedConsumer(
-    channel,
-    sendNotification
-  );
-
-  const userWelcomeConsumer = new UserWelcomeConsumer(
-    channel,
-    sendNotification
-  );
-
   const serviceNotificationConsumer = new ServiceNotificationConsumer(
     channel,
     generateTokenForUser,
     sendNotification
   );
 
-  await userCreatedConsumer.consume();
-  await contactCreatedConsumer.consume();
-  await userWelcomeConsumer.consume();
   await serviceNotificationConsumer.consume();
 };
