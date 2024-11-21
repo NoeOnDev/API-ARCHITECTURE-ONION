c649cb8cf65dc4b059f20f22c207b465e8c541c0
NoeOnDev <alxg5516@gmail.com>
2024-11-20 20:54:06 -0600

    feat: mejorar esquemas de validación; agregar restricciones de longitud y formato para nombres, teléfono y hobby

--- bounded-contexts/notifications/src/tokens/infrastructure/validationSchemas.ts
+++ bounded-contexts/notifications/src/tokens/infrastructure/validationSchemas.ts
@@ -4,8 +4,5 @@ export const validateTokenSchema = Joi.object({
   code: Joi.string()
     .pattern(/^\d{5}$/, { name: "5 digits" })
     .trim()
-    .required()
-    .messages({
-      "string.pattern.name": '"code" must be a 5-digit number',
-    }),
+    .required(),
 });
--- bounded-contexts/users/src/contacts/infrastructure/validationSchemas.ts
+++ bounded-contexts/users/src/contacts/infrastructure/validationSchemas.ts
@@ -1,9 +1,25 @@
 import Joi from "joi";
 
 export const saveContactSchema = Joi.object({
-  firstName: Joi.string().trim().required(),
-  lastName: Joi.string().trim().required(),
+  firstName: Joi.string()
+    .min(2)
+    .max(50)
+    .trim()
+    .regex(/^[a-zA-Z]+$/)
+    .required(),
+  lastName: Joi.string()
+    .min(2)
+    .max(50)
+    .trim()
+    .regex(/^[a-zA-Z]+$/)
+    .required(),
   email: Joi.string().trim().email().required(),
-  phone: Joi.string().trim().required(),
-  hobby: Joi.string().trim().required(),
+  phone: Joi.string()
+    .trim()
+    .regex(/^\+?[1-9]\d{1,14}$/)
+    .required(),
+  hobby: Joi.string()
+    .trim()
+    .regex(/^[a-zA-Z]+$/)
+    .required(),
 });
